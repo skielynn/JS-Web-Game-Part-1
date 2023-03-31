786c9b6796f4328eab7c3aa77418787e7cb7b930
Skie <skielynn.1105945@gmail.com>
2023-03-31 10:45:54 -0700

    added images w newImge function

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
 
 newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
