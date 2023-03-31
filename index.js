32d51eeca685151775956ae1d84160efd255ae29
Skie <skielynn.1105945@gmail.com>
2023-03-31 10:36:31 -0700

    added pine tree

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
